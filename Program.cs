9b4e045a95446eda1f8c198faacdb8017f5100f7
Gianna Smith <gnsmith127@gmail.com>
2021-12-08 15:47:23 -0500

    Finished Guessing Game loop

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var r = new Random();
+            var favNumber = r.Next(1,1000);
+
+            Console.WriteLine("Try to guess my favorite number.");
+            int userInput = 0;
+
+            while (userInput != favNumber)
+            {
+                Console.WriteLine("Please enter a guess between 1 and 1000.");
+                userInput = Convert.ToInt32(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Your guess is too low. Try again.");
+                    
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Your guess is too high. Try again");
+                 
+                }
+                else
+                {
+                    Console.WriteLine($"{favNumber} is the correct answer! Congratulations!");
+                }
+            
+            }
+
+            
+            
+        }
+    }
+}
